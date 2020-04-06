a89aa68685f98924a2af617b21928c84df205dee
lulzwuut <max.velly@gmail.com>
2020-04-06 18:01:27 +0200

    vanilla

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -87,13 +87,54 @@ function setGallery(el) {
 		document.getElementById('prev').addEventListener("click", function() {
 			gallery_elements[prevkey].click();
 		});
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
 
-        $('#lightbox').on("swiperight", function() {  
-            gallery_elements[prevkey].click();
-        });
-        $('#lightbox').on("swipeleft", function() {
-            gallery_elements[nextkey].click();
-        });
+        var xDown = null;                                                        
+        var yDown = null;
+
+        function getTouches(evt) {
+          return evt.touches ||             // browser API
+                 evt.originalEvent.touches; // jQuery
+        }                                                     
+
+        function handleTouchStart(evt) {
+            const firstTouch = getTouches(evt)[0];                                      
+            xDown = firstTouch.clientX;                                      
+            yDown = firstTouch.clientY;                                      
+        };                                                
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    /* left swipe */ 
+                    gallery_elements[nextkey].click();
+
+                } else {
+                    /* right swipe */
+                    gallery_elements[prevkey].click();
+                }                       
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */ 
+                } else { 
+                    /* down swipe */
+                }                                                                 
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
+        };
 	}
 }
 
